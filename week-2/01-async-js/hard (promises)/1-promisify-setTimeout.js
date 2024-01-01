b57e078c87d1a2440849e679577f562145fb964a
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-01-01 14:12:51 +0530

    promisify-setTimeout

--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -1,8 +1,33 @@
 /*
     Write a function that returns a promise that resolves after n seconds have passed, where n is passed as an argument to the function.
+
+    // Wrapping async function
+    function myOwnSetTimeout(fn,duration) {
+        setTimeout(fn,duration);
+    }
+    myOwnSetTimeout(function(){
+        console.log('myOwnSetTimeout')
+    },1000);
+
+    // Promisify
+    function myOwnSetTimeoutPromise(duration) {
+        let p = new Promise(function(resolve,reject) {
+            // after duration seconds call resolve
+            setTimeout(resolve,duration);
+        });
+        return p;
+    }
+    myOwnSetTimeoutPromise(1000)
+    .then(function() {
+        console.log('myOwnSetTimeoutPromise');
+    })
 */
 
 function wait(n) {
+  let p = new Promise(function (resolve, reject) {
+    setTimeout(resolve, n * 1000);
+  });
+  return p;
 }
 
 module.exports = wait;
