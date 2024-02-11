6861211e04d56e0a3ed9bacae8f50c2820b9a341
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-02-11 15:41:19 +0530

    useMemo Assignments

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -1,27 +1,40 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo } from "react";
 // You have been given a list of items you shopped from the grocery store
 // You need to calculate the total amount of money you spent
 
 export const Assignment3 = () => {
-    const [items, setItems] = useState([
-        { name: 'Chocolates', value: 10 },
-        { name: 'Chips', value: 20 },
-        { name: 'Onion', value: 30 },
-        { name: 'Tomato', value: 30 },
-        // Add more items as needed
-    ]);
+  const [items, setItems] = useState([
+    { name: "Chocolates", value: 10 },
+    { name: "Chips", value: 20 },
+    { name: "Onion", value: 30 },
+    { name: "Tomato", value: 30 },
+    { name: "Potato", value: 130 },
+    { name: "Dhaniya", value: 130 },
+    // Add more items as needed
+  ]);
 
-    // Your code starts here
-    const totalValue = 0;
-    // Your code ends here
-    return (
-        <div>
-            <ul>
-                {items.map((item, index) => (
-                    <li key={index}>{item.name} - Price: ${item.value}</li>
-                ))}
-            </ul>
-            <p>Total Value: {totalValue}</p>
-        </div>
-    );
+  // Your code starts here
+  const totalValue = useMemo(() => {
+    let totalSpent = 0;
+    let total = items.reduce((totalSpent, item) => {
+      return (totalSpent += item.value);
+    }, totalSpent);
+    /* items.map((item) => {
+      totalSpent += item.value;
+    }); */
+    return total;
+  }, [items]);
+  // Your code ends here
+  return (
+    <div>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item.name} - Price: ${item.value}
+          </li>
+        ))}
+      </ul>
+      <p>Total Value: {totalValue}</p>
+    </div>
+  );
 };
