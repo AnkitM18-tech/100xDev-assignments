fd1acd9f7565c76b13b365b1fc58e7b340259507
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2024-02-11 17:59:41 +0530

    useRef Assignments

--- week-6/3-use-ref/src/components/Assignment1.jsx
+++ week-6/3-use-ref/src/components/Assignment1.jsx
@@ -1,21 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 // Create a component with a text input field and a button. When the component mounts or the button is clicked, automatically focus the text input field using useRef.
 
 export function Assignment1() {
+  const inputRef = useRef();
+  useEffect(() => {
+    // document.getElementById("inputBox").focus(); -> not so good looking code, so we can use useRef() instead. -> useCallback can be another option.
+    inputRef.current.focus();
+  }, [inputRef]);
 
-    useEffect(() => {
+  const handleButtonClick = () => {
+    // document.getElementById("inputBox").focus();
+    inputRef.current.focus();
+  };
 
-    }, []);
-
-    const handleButtonClick = () => {
-
-    };
-
-    return (
-        <div>
-            <input type="text" placeholder="Enter text here" />
-            <button onClick={handleButtonClick}>Focus Input</button>
-        </div>
-    );
-};
+  return (
+    <div>
+      <input
+        type="text"
+        id="inputBox"
+        ref={inputRef}
+        placeholder="Enter text here"
+      />
+      <button onClick={handleButtonClick}>Focus Input</button>
+    </div>
+  );
+}
--- week-6/3-use-ref/src/components/Assignment2.jsx
+++ week-6/3-use-ref/src/components/Assignment2.jsx
@@ -1,19 +1,25 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useRef } from "react";
 
 // Create a component that tracks and displays the number of times it has been rendered. Use useRef to create a variable that persists across renders without causing additional renders when it changes.
 
+// let numberOfTimesRender = 0; -> can be one approach, but should not use it. No global variables should be used.
 export function Assignment2() {
-    const [, forceRender] = useState(0);
+  const [, forceRender] = useState(0);
+  //   const [noOfReRenders, setNoOfReRenders] = useState(0); => for this usecase it will be the worst way because it will render twice since we are updating noOfReRenders using a state variable. so when we change it it will render once for forceRender and again for noOfRenders state variable.
 
-    const handleReRender = () => {
-        // Update state to force re-render
-        forceRender(Math.random());
-    };
+  const noOfRenders = useRef(0); // another usecase of useRef -> we can not only store DOM element references but also numbers and other things as well. -> it is like having a global variable outside the component which doesn't reset to 0 / overwriting it.
 
-    return (
-        <div>
-            <p>This component has rendered {0} times.</p>
-            <button onClick={handleReRender}>Force Re-render</button>
-        </div>
-    );
-};
+  const handleReRender = () => {
+    // Update state to force re-render
+    forceRender(Math.random());
+  };
+  //   numberOfTimesRender += 1;
+  noOfRenders.current = noOfRenders.current + 1;
+
+  return (
+    <div>
+      <p>This component has rendered {noOfRenders.current} times.</p>
+      <button onClick={handleReRender}>Force Re-render</button>
+    </div>
+  );
+}
